d8f98d04dcd58967be755993cc7ba06bd5d11f04
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-04-22 20:24:44 -0300

    CSS al Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Navegacion from './navegacion';
+import {css} from '@emotion/core';
 
 const Header = () => {
     return ( 
-        <header>
-            <div>
+        <header
+            css={css `
+                background-color: #0D283B;
+                padding: 1rem;
+            `}
+        >
+            <div
+                css={css `
+                max-width: 120rem;
+                margin: 0 auto;
+                text-align: center;
+
+                @media (min-width: 768px) {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                }
+                    
+            `}>
                 <Link>
                     Bienes Raices
                 </Link>
