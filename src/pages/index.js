7ec2f925a5a11b9729b7f5a8826d28754a21f7be
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-04-22 13:52:09 -0300

    Creando un Layout Principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Layout from '../components/layout'
 
 const Index = () => {
     return ( 
-        <h1>Index</h1>
+        <Layout>
+            <h1>Index</h1>
+        </Layout>
+
      );
 }
  
