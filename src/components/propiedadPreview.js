c72e8dcaea4b61471ccfab18c3e59cf6b73d7afa
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-04-23 20:02:04 -0300

    Mostrando información de las Propiedades

--- src/components/propiedadPreview.js
+++ src/components/propiedadPreview.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Iconos from './iconos';
 
 const PropiedadPreview = ({propiedad}) => {
-    const { nombre } = propiedad;
+    const { nombre, descripcion, imagen, wc, estacionamiento, habitaciones, precio } = propiedad;
     return ( 
-        <h3>{nombre}</h3>
+        <div>
+            <div>
+                <h3>{nombre}</h3>
+
+                <p>{precio}</p>
+
+                <Iconos 
+                    wc={wc}
+                    estacionamiento={estacionamiento}
+                    habitaciones={habitaciones}
+                />
+            </div>
+        </div>
      );
 }
  
