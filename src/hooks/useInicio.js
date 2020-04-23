3fc50b43adb5965e4091edf7d01011a917a5f8f4
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-04-22 21:51:17 -0300

    Consultando en GraphQL para obtener la página de inicio

---
+++ src/hooks/useInicio.js
@@ -0,0 +1,26 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const useInicio = () => {
+    const resultado = useStaticQuery(
+        graphql`
+            query {
+                allStrapiPaginas( filter: {nombre: { eq: "Inicio"}}) {
+                nodes {
+                    id
+                    nombre
+                    contenido
+                    imagen {
+                    publicURL
+                    }
+                }
+                }
+            }
+          
+        `
+
+    );
+
+    return resultado;
+}
+ 
+export default useInicio;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import Layout from '../components/layout'
+import Layout from '../components/layout';
+import useInicio from '../hooks/useInicio';
 
 const Index = () => {
+
+    const inicio = useInicio();
     return ( 
         <Layout>
             <h1>Index</h1>
