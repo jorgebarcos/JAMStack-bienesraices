4914f06d015e3f1b20a14be263c707b4aa8fbee2
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-05-06 01:59:18 -0300

    Agregando un select para filtrar las propiedades

--- src/components/listadoPropiedades.js
+++ src/components/listadoPropiedades.js
@@ -3,11 +3,15 @@ import { css } from '@emotion/core';
 import usePropiedades from '../hooks/usePropiedades';
 import PropiedadPreview from './propiedadPreview';
 import listadoPropiedadesCSS from '../css/listadoPropiedades.module.css';
+import useFiltro from '../hooks/useFiltro';
 
 const ListadoPropiedades = () => {
 
     const resultado = usePropiedades();
     const [propiedades, guardarPropiedades] = useState([]);
+
+    // Filtrado de propiedades
+    const { FiltroUI } = useFiltro();
 
     useEffect(() => {
         guardarPropiedades(resultado);
@@ -20,6 +24,8 @@ const ListadoPropiedades = () => {
             <h2 css={css `
                 margin-top: 5rem;
             `}>Nuestras Propiedades</h2>
+
+            {FiltroUI()}
 
             <ul className={listadoPropiedadesCSS.propiedades}>
                 {propiedades.map( propiedad => (
