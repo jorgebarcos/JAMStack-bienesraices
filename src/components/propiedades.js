4ba259f9fbfbefe93856bcacd3358a74f3358cf3
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-05-06 01:51:43 -0300

    CSS a las propiedades

--- src/components/propiedades.js
+++ src/components/propiedades.js
@@ -5,6 +5,35 @@ import Iconos from './iconos';
 import Layout from './layout';
 import { graphql } from 'gatsby'
 
+const Contenido = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 95%;
+
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        column-gap: 5rem
+    }
+`
+
+const Sidebar = styled.aside`
+    .precio {
+        font-size: 2rem;
+        color: #75AB00;
+    }
+    .agente {
+        margin-top: 4rem;
+        border-radius: 2rem;
+        background-color: #75AB00;
+        padding: 3rem;
+        color: #FFFF;
+    }
+    p {
+        margin: 0;
+    }
+`
+
 export const query = graphql`
     query($id: String!) {
             allStrapiPropiedades( filter: { id: { eq: $id } }) {
@@ -42,28 +71,28 @@ export const query = graphql`
     return ( 
         <Layout>
             <h1>{nombre}</h1>
-            <div>
+            <Contenido>
                 <main>
                     <Image
                         fluid={imagen.sharp.fluid}
                     />
                     <p>{descripcion}</p>
                 </main>
-                <aside>
-                    <p>$ {precio}</p>
+                <Sidebar>
+                    <p className="precio">$ {precio}</p>
                     <Iconos
                     wc={wc}
                     estacionamiento={estacionamiento}
                     habitaciones={habitaciones}
                     />
-                    <div>
+                    <div className="agente">
                         <h2>Vendedor:</h2>
                         <p>{agente.nombre}</p>
                         <p>Tel: {agente.telefono}</p>
                         <p>Email: {agente.email}</p>
                     </div>
-                </aside>
-            </div>
+                </Sidebar>
+            </Contenido>
         </Layout>
      );
 }
