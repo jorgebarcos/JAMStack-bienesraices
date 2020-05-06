5baa1ba8f7cc6fecc8b0a4035faca3bbf8449316
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-05-06 11:23:15 -0300

    Obteniendo las opciones para filtrar de forma dinamica

--- src/hooks/useFiltro.js
+++ src/hooks/useFiltro.js
@@ -1,10 +1,28 @@
 import React, {useState} from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const  useFiltro = () => {
+
+    const resultado = useStaticQuery(graphql`
+    query {
+        allStrapiCategorias {
+          nodes {
+            nombre
+            id
+          }
+        }
+      }      
+    `);
+
+    const categorias = resultado.allStrapiCategorias.nodes;
+
     const FiltroUI = () => (
         <form>
             <select>
                 <option value="">-- Filtrar --</option>
+                {categorias.map(opcion => (
+                    <option key={opcion.id} value={opcion.nombre}>{opcion.nombre}</option>
+                ))}
             </select>
         </form>
     )
