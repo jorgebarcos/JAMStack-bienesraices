e09beb10000acad133052d594a11b482282f4280
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-05-06 00:46:08 -0300

    Consultando las propiedades por su ID

--- src/components/propiedades.js
+++ src/components/propiedades.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import Image from 'gatsby-image';
+import Iconos from './iconos';
+import Layout from './layout';
+import { graphql } from 'gatsby'
 
-const Propiedades = () => {
+export const query = graphql`
+    query($id: String!) {
+        allStrapiPropiedades( filter: { id: { eq: $id } }) {
+            nodes {
+                nombre
+                estacionamiento
+            }
+        }
+    }
+`
+
+
+const Propiedades = ({data}) => {
+
+    
+
     return ( 
-        <h1>Propiedad</h1>
+        <Layout>
+            <h1>Renzo</h1>
+        </Layout>
      );
 }
  
